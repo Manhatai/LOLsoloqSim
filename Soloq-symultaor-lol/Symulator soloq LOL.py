6ff30a3702b05324d7bfdaf710cd44253b982ed6
Manhatai <matixxx.mp4@gmail.com>
2023-10-17 20:11:16 +0200

    Added: - rewrote "if rank_current" into "rank_types" dict - rewrote "rank_gained" into dict instead of a bunch of if statements

--- Soloq-symultaor-lol/Symulator soloq LOL.py
+++ Soloq-symultaor-lol/Symulator soloq LOL.py
@@ -32,128 +32,25 @@ for i in range(1000):
 result = sum(lp_total) / 1000
 result_whole = round(result)
 
-if rank_current == "iron IV":  
-    result_whole = result_whole + lp_count + 0
-elif rank_current == "iron III":
-    result_whole = result_whole + lp_count + 100
-elif rank_current == "iron II":
-    result_whole = result_whole + lp_count + 200
-elif rank_current == "iron I":
-    result_whole = result_whole + lp_count + 300
-elif rank_current == "bronze IV":
-    result_whole = result_whole + lp_count + 400
-elif rank_current == "bronze III":
-    result_whole = result_whole + lp_count + 500
-elif rank_current == "bronze II":
-    result_whole = result_whole + lp_count + 600
-elif rank_current == "bronze I":
-    result_whole = result_whole + lp_count + 700
-elif rank_current == "silver IV":
-    result_whole = result_whole + lp_count + 800
-elif rank_current == "silver III":
-    result_whole = result_whole + lp_count + 900
-elif rank_current == "silver II":
-    result_whole = result_whole + lp_count + 1000
-elif rank_current == "silver I":
-    result_whole = result_whole + lp_count + 1100
-elif rank_current == "gold IV":
-    result_whole = result_whole + lp_count + 1200
-elif rank_current == "gold III":
-    result_whole = result_whole + lp_count + 1300
-elif rank_current == "gold II":
-    result_whole = result_whole + lp_count + 1400
-elif rank_current == "gold I":
-    result_whole = result_whole + lp_count + 1500
-elif rank_current == "platinum IV":
-    result_whole = result_whole + lp_count + 1600
-elif rank_current == "platinum III":
-    result_whole = result_whole + lp_count + 1700
-elif rank_current == "platinum II":
-    result_whole = result_whole + lp_count + 1800
-elif rank_current == "platinum I":
-    result_whole = result_whole + lp_count + 1900
-elif rank_current == "emerald IV":
-    result_whole = result_whole + lp_count + 2000
-elif rank_current == "emerald III":
-    result_whole = result_whole + lp_count + 2100
-elif rank_current == "emerald II":
-    result_whole = result_whole + lp_count + 2200
-elif rank_current == "emerald I":
-    result_whole = result_whole + lp_count + 2300
-elif rank_current == "diamond IV":
-    result_whole = result_whole + lp_count + 2400
-elif rank_current == "diamond III":
-    result_whole = result_whole + lp_count + 2500
-elif rank_current == "diamond II":
-    result_whole = result_whole + lp_count + 2600
-elif rank_current == "diamond I":
-    result_whole = result_whole + lp_count + 2700
+try:
+    rank_types = {"iron IV": 0, "iron III": 100, "iron II": 200, "iron I": 300, "bronze IV": 400, "bronze III": 500, "bronze II": 600, "bronze I": 700, "silver IV": 800, "silver III": 900, "silver II": 1000, "silver I": 1100, "gold IV": 1200, "gold III": 1300, "gold II": 1400, "gold I": 1500,"platinum IV": 1600, "platinum III": 1700, "platinum II": 1800, "platinum I": 1900, "emerald IV": 2000, "emerald III": 2100, "emerald II": 2200, "emerald I": 2300, "diamond IV": 2400, "diamond III": 2500, "diamond II": 2600, "diamond I": 2700 }
+    result_whole += lp_count + rank_types.get(rank_current)
+except TypeError:
+    print("Wpisz poprawną nazwę rangi.")
+    quit()
 
 
-def rank_gained(result_whole):  
-    if result_whole < 0:
-        print("Your rank should be: iron IV")
-    if result_whole in range(0, 99):
-        print("Your rank should be: iron IV")
-    if result_whole in range(100, 199):
-        print("Your rank should be: iron III")
-    if result_whole in range(200, 299):
-        print("Your rank should be: iron II")
-    if result_whole in range(300, 399):
-        print("Your rank should be: iron I")
-    if result_whole in range(400, 499):
-        print("Your rank should be: bronze IV")
-    if result_whole in range(500, 599):
-        print("Your rank should be: bronze III")
-    if result_whole in range(600, 699):
-        print("Your rank should be: bronze II")
-    if result_whole in range(700, 799):
-        print("Your rank should be: bronze I")
-    if result_whole in range(800, 899):
-        print("Your rank should be: silver IV")
-    if result_whole in range(900, 999):
-        print("Your rank should be: silver III")
-    if result_whole in range(1000, 1099):
-        print("Your rank should be: silver II")
-    if result_whole in range(1100, 1199):
-        print("Your rank should be: silver I")
-    if result_whole in range(1200, 1299):
-        print("Your rank should be: gold IV")
-    if result_whole in range(1300, 1399):
-        print("Your rank should be: gold III")
-    if result_whole in range(1400, 1499):
-        print("Your rank should be: gold II")
-    if result_whole in range(1500, 1599):
-        print("Your rank should be: gold I")
-    if result_whole in range(1600, 1699):
-        print("Your rank should be: platinum IV")
-    if result_whole in range(1700, 1799):
-        print("Your rank should be: platinum III")
-    if result_whole in range(1800, 1899):
-        print("Your rank should be: platinum II")
-    if result_whole in range(1900, 1999):
-        print("Your rank should be: platinum I")
-    if result_whole in range(2000, 2099):
-        print("Your rank should be: emerald IV")
-    if result_whole in range(2100, 2199):
-        print("Your rank should be: emerald III")
-    if result_whole in range(2200, 2299):
-        print("Your rank should be: emerald II")
-    if result_whole in range(2300, 2399):
-        print("Your rank should be: emerald I")
-    if result_whole in range(2400, 2499):
-        print("Your rank should be: diamond IV")
-    if result_whole in range(2500, 2599):
-        print("Your rank should be: diamond III")
-    if result_whole in range(2600, 2699):
-        print("Your rank should be: diamond II")
-    if result_whole in range(2700, 2799):
-        print("Your rank should be: diamond I")
-    if result_whole > 2800:
-        print("Your rank should be: master +")
+
+def rank_gained(result_whole):
+    rank_ranges = { (0, 99): "iron IV", (100, 199): "iron III", (200, 299): "iron II", (300, 399): "iron I", (400, 499): "bronze IV", (500, 599): "bronze III", (600, 699): "bronze II", (700, 799): "bronze I", (800, 899): "silver IV", (900, 999): "silver III", (1000, 1099): "silver II", (1100, 1199): "silver I", (1200, 1299): "gold IV", (1300, 1399): "gold III", (1400, 1499): "gold II", (1500, 1599): "gold I", (1600, 1699): "platinum IV", (1700, 1799): "platinum III",(1800, 1899): "platinum II", (1900, 1999): "platinum I", (2000, 2099): "emerald IV", (2100, 2199): "emerald III", (2200, 2299): "emerald II", (2300, 2399): "emerald I", (2400, 2499): "diamond IV", (2500, 2599): "diamond III", (2600, 2699): "diamond II", (2700, 2799): "diamond I", }
+    for (start, end), rank in rank_ranges.items():
+        if start <= result_whole <= end:
+            print(f"Your rank should be: {rank}")
+        else:
+            print("Your rank should be: master +")
+
 
 
 rank_gained(result_whole)
-
 print(f"Your total lp after 1000 games: {result_whole}")
 
